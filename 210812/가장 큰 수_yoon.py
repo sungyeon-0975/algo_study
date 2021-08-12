1bc68015db8b350c81f48baccf15709d01140fb5
jiyoon <jiyoonhan12@gmail.com>
2021-08-12 20:41:56 +0900

    0812 yoon mod

--- 210812/가장 큰 수_yoon.py
+++ 210812/가장 큰 수_yoon.py
@@ -2,13 +2,14 @@ def solution(numbers):
     numbers = list(map(str, numbers))
     comp = []
     for num in numbers:
-        num_3 = num*3
-        comp.append([num_3[:3], num])
-    comp.sort(key = lambda x : int(x[0]), reverse=True)
+        num_3 = num * 3
+        comp.append([num_3, num])
+    
+    comp.sort(key = lambda x : x[0], reverse=True) # int로 안 감싸줘도 str는 사전식으로 검사하기 때문에 잘 나옴
     
     answer = ''
     for idx in range(len(numbers)):
         answer += comp[idx][1]
-    return answer
+    return str(int(answer)) # 0000 처리해주기 위해 int로 한번 감싸고 str
 
-print(solution([6, 10, 2])) # 예시 케이스는 통과, 전체 케이스 중 4개만 통과
+print(solution([6, 10, 2]))
