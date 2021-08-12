434823cf6e86e3536399fced3911713964231cf6
sungyeon-0975 <sustain98@naver.com>
2021-08-12 17:32:47 +0900

    추가 풀이

--- 210812/가장 큰 수_sungyeon.py
+++ 210812/가장 큰 수_sungyeon.py
@@ -11,3 +11,27 @@ def solution(numbers):
     n = sorted(n, key=functools.cmp_to_key(comparator))
     answer = str(int(''.join(n)))
     return answer
+
+
+# from collections import deque
+
+# def solution(numbers):
+#     l=deque(sorted(list(map(str,numbers)), key=lambda x: (x[0],converse(x),converse1(x))))
+#     # print(l)
+#     answer = int(merge1(l))
+#     return str(answer)
+
+# def converse(x):
+#     y=x[0]
+#     return (x+y+y+y)[:5]
+
+# def converse1(x):
+#     y=x[len(x)-1]
+#     return (x+y+y+y)[:5]
+
+# def merge1(l):
+#     s=''
+#     while l:
+#         x=l.popleft()
+#         s=x+s
+#     return s
