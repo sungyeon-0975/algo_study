20a5f403dc03fc11f96dc792a5f1557abefa0362
jiyoon <yoonnotyoun@gmail.com>
2021-08-19 21:59:37 +0900

    0819 yoon after study

--- 210819/10157_yoon.py
+++ 210819/10157_yoon.py
@@ -19,16 +19,12 @@ else:
     while num <= K: # 달팽이 채워주기
         ni = i + di[w]
         nj = j + dj[w]
-        valid = 0 <= ni < C and 0 <= nj < R
-        if valid and arr[ni][nj] == 0:
+
+        if 0 <= ni < R and 0 <= nj < C and arr[ni][nj] == 0:
             arr[ni][nj] = num
             num += 1
             i, j = ni, nj
         else:
             w = (w+1) % 4
 
-    if arr[i][j] == K:
-        #print(i, j) # 행, 열 값이 각각 위치 바뀌고 기존 열+1, 행은 R-i
-        print(j+1, R-i)
-
-# 시간초과남... 제법 성질나요
+    print(j+1, R-i)
