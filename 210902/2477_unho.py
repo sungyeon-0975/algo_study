ff268bcf5be90bfeaaad3d1aa2b9cb82020fe3cb
unho <djsgh94@naver.com>
2021-08-29 16:03:22 +0900

    unho add

--- 210902/2477_unho.py
+++ 210902/2477_unho.py
@@ -2,8 +2,20 @@ import sys
 sys.stdin = open('input_2477.txt')
 
 K = int(input())
+num_list = [list(map(int, input().split())) for _ in range(6)]
 
-total = 1
-len_list = [[] for _ in range(5)]
-for i in range(6):
-    direction, length = map(int, input().split())
+max_width = max(filter(lambda x: x[0]==1 or x[0]==2, num_list), key=lambda x: x[1])
+max_height = max(filter(lambda x: x[0]==3 or x[0]==4, num_list), key=lambda x: x[1])
+
+max_width_idx = num_list.index(max_width)
+max_height_idx = num_list.index(max_height)
+
+if (max_width_idx + 1) % 6 == max_height_idx:
+    minus_width_idx = (max_width_idx + 4) % 6
+    minus_height_idx = (max_width_idx + 3) % 6
+else:
+    minus_width_idx = (max_width_idx + 2) % 6
+    minus_height_idx = (max_width_idx + 3) % 6
+
+answer = (max_width[1] * max_height[1]) - (num_list[minus_width_idx][1] * num_list[minus_height_idx][1])
+print(answer * K)
