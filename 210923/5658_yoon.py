d7d6b76e3f780cdb393280c1e391825e0987d82d
jiyoon <yoonnotyoun@gmail.com>
2021-09-22 20:47:44 +0900

    0922 yoon

---
+++ 210923/5658_yoon.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+def addingWords(line):
+    global num
+    for i in range(4):
+        word = line[case*i:case*(i+1)]
+        num.add(int(word, 16))
+
+
+T = int(input())
+
+for t in range(1, T+1):
+    N, K = map(int, input().split())
+    # 한 칸에 들어가는 숫자 수 (중복 나오지 않는)
+    case = N//4
+    num = set()
+    line = input()
+
+    for _ in range(case):
+        line = line[-1:] + line[:-1]
+        addingWords(line)
+
+    num = sorted(list(num), reverse=True)
+    print(f'#{t} {num[K-1]}')
