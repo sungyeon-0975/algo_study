0b72a9616a8f66d16d0b8e974166b6d892a90af3
jiyoon <yoonnotyoun@gmail.com>
2021-09-30 23:24:16 +0900

    0930 daily

--- 210930/5639_yoon_미해결.py
+++ 210930/5639_yoon_미해결.py
@@ -1,17 +1,23 @@
 import sys
 # input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 sys.stdin = open('input_5639.txt')
 
-'''
-아무래도 테스트케이스만 맞는듯
-'''
 
-def post_order(node):
-    if left[node]:
-        post_order(left[node])
-    if right[node]:
-        post_order(right[node])
-    print(pre_order[node])
+def post_order(tree):
+    if tree:
+        root = tree[0]
+        idx = -1
+        for i in range(1, len(tree)):
+            if tree[i] > root:
+                idx = i
+                break
+        if idx == -1:
+            post_order(tree[1:])
+        else:
+            post_order(tree[1:idx])
+            post_order(tree[idx:])
+        print(root)
 
 
 pre_order = []
@@ -21,21 +27,4 @@ while True:
     except:
         break
 
-N = len(pre_order)
-left = [0] * N
-right = [0] * N
-
-for n in range(1, N):
-    if pre_order[n] < pre_order[n-1]:
-        left[n-1] = n
-    else:
-        for k in range(n-1):
-            if pre_order[k+1] < pre_order[n] < pre_order[k]:
-                right[k+1] = n
-                break
-            elif right[k] == 0 and pre_order[0] <= pre_order[k] < pre_order[n]:
-                right[k] = n
-                break
-
-
-post_order(0)
+post_order(pre_order)
