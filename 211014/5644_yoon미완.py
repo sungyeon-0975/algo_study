8a61a3ca43525aaa0f5e008e5b74f3a03675ead8
jiyoon <yoonnotyoun@gmail.com>
2021-10-14 22:25:01 +0900

    1014 yoon

--- 211014/5644_yoon미완.py
+++ 211014/5644_yoon미완.py
@@ -4,36 +4,38 @@ sys.stdin = open('input_5644.txt')
 # 이동x / 상 / 우 / 하 / 좌
 dir = {0:(0, 0), 1:(-1, 0), 2:(0, 1), 3:(1, 0), 4:(0, -1)}
 
-def check(a, b, step, temp):
-    global charge
-    if step > M:
-        charge = temp
-        return
-    ai, aj = a
-    bi, bj = b
-    na = (ai+dir[am[step]][0], aj+dir[am[step]][1])
-    nb = (bi+dir[bm[step]][0], bj+dir[bm[step]][1])
-    # print(temp)
-    check(na, nb, step+1, temp + BC[arr[a[0]][a[1]]] + BC[arr[b[0]][b[1]]])
-
-
 T = int(input())
 for t in range(1, T+1):
     M, A = map(int, input().split())
-    am = list(map(int, input().split()))
-    bm = list(map(int, input().split()))
-    arr = [[0] * 11 for _ in range(11)]
-    # print(arr)
-    BC = [0]
-    charge = 0
-    for idx in range(1, A+1):
-        i, j, cov, p = map(int, input().split())
-        for r in range(1, 11):
-            for c in range(1, 11):
-                if abs(r + c - i - j) <= cov:
-                    arr[r][c] = cov
-        BC.append(p)
-    # print(arr)
-    a, b = (1, 1), (10, 10)
-    check(a, b, 0, 0)
-    print(charge)
+    am = list(map(int, input().split())) + [0]
+    bm = list(map(int, input().split())) + [0]
+    BC = []
+    ans = 0
+    for _ in range(A):
+        BC.append(list(map(int, input().split())))
+    BC.sort(key = lambda x:x[3], reverse=True)
+    # print(BC)
+
+    ax, ay, bx, by = 1, 1, 10, 10
+    for i in range(M+1):
+        select = [0, 0, 0]
+        cnt = 0
+        for j in range(A):
+            cnt += 1
+            if not select[2] and ((abs(BC[j][0] - ay) + abs(BC[j][1] - ax)) <= BC[j][2]) and ((abs(BC[j][0] - by) + abs(BC[j][1] - bx)) <= BC[j][2]):
+                select[2] = BC[j][3]
+            elif not select[0] and ((abs(BC[j][0] - ay) + abs(BC[j][1] - ax)) <= BC[j][2]):
+                select[0] = BC[j][3]
+            elif not select[1] and ((abs(BC[j][0] - by) + abs(BC[j][1] - bx)) <= BC[j][2]):
+                select[1] = BC[j][3]
+            else:
+                cnt -= 1
+            if cnt == 2:
+                break
+        ans += sum(select)
+        ax += dir[am[i]][0]
+        ay += dir[am[i]][1]
+        bx += dir[bm[i]][0]
+        by += dir[bm[i]][1]
+
+    print('#{} {}'.format(t, ans))
