bb79843dad39933a4320fbf57befd6a66c7bc0ab
jiyoon <yoonnotyoun@gmail.com>
2021-11-13 00:01:58 +0900

    1113 yoon

---
+++ 211202/문자열압축_yoon.py
@@ -0,0 +1,23 @@
+def solution(s):
+    answer = len(s)
+    for i in range(1, len(s)//2 + 1):
+        temp = ''
+        cnt = 1
+        for j in range(1, len(s)//i + 2):
+            if s[i*(j-1):i*j] == s[i*j:i*(j+1)]:
+                cnt += 1
+            else:
+                if cnt != 1:
+                    temp += s[i*(j-1):i*j] + str(cnt)
+                else:
+                    temp += s[i*(j-1):i*j]
+                cnt = 1
+        # for? len(s)//i 곱해서 단위별로 같으면 누적... 아귀찮아
+        answer = min(answer, len(temp))
+    return answer
+
+print(solution("aabbaccc"))
+print(solution("ababcdcdababcdcd"))
+print(solution("abcabcdede"))
+print(solution("abcabcabcabcdededededede"))
+print(solution("xababcdcdababcdcd"))
