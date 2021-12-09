888232933a67a55d01d2fcd5b34adcb9f4ec90ee
Aaronryun <Aaronryun@gmail.com>
2021-12-09 16:51:42 +0900

    rk

--- 211209/2468_ryunki.py
+++ 211209/2468_ryunki.py
@@ -1,3 +1,6 @@
+"""
+31880kb, 1724ms
+"""
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -17,8 +20,26 @@ def bfs(x,y,cnt):
         for i in range(4):
             nx = x+dx[i]
             ny=y+dy[i]
-            if 0 <= nx < N and 0<=ny<N
+            if 0 <= nx < N and 0<=ny<N:
+                if data[nx][ny] >= cnt and visited[nx][ny] ==0:
+                    visited[nx][ny]=1
+                    queu.append((nx,ny))
 
 
 N = int(input())
 data = list(list(map(int,input().split())) for _ in range(N))
+
+
+answer = min(map(min,data)) 
+
+for x in range(min(map(min,data)), max(map(max,data))+1):
+    visited = [[0]*N for _ in range(N)]
+    temp = 0
+    for i in range(N):
+        for j in range(N):
+            if data[i][j]>=x and visited[i][j] == 0:
+                bfs(i,j,x)
+                temp += 1
+    if temp > answer:
+        answer = temp
+print(answer)
