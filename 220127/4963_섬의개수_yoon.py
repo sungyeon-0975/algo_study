ac5350238461dacb35356a65c005df42992ce81b
jiyoon <yoonnotyoun@gmail.com>
2022-01-23 01:27:04 +0900

    0123 yoon

--- 220127/4963_섬의개수_yoon.py
+++ 220127/4963_섬의개수_yoon.py
@@ -4,6 +4,10 @@ sys.stdin = open('4963_input.txt')
 
 while True:
     w, h = map(int, input().split())
+    ans = 0
     if w == 0 and h == 0:
         break
     arr = [[0] * h for _ in range(w)]
+    for i in range(w):
+        for j in range(h):
+            pass
