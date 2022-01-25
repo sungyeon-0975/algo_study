2a56a883378faf07972661269e805b4144abe9be
jiyoon <yoonnotyoun@gmail.com>
2022-01-26 00:33:24 +0900

    0126 yoon

--- 220127/4963_섬의개수_yoon.py
+++ 220127/4963_섬의개수_yoon.py
@@ -1,13 +1,35 @@
 import sys
+sys.setrecursionlimit(10000)
 sys.stdin = open('4963_input.txt')
 # input = sys.stdin.readline
 
+
+# 33152KB / 88ms
+
+
+def dfs(r, c):
+    for k in range(8):
+        ni = r + di[k]
+        nj = c + dj[k]
+        if 0 <= ni < h and 0 <= nj < w and arr[ni][nj] and not visited[ni][nj]:
+            visited[ni][nj] = 1
+            dfs(ni, nj)
+
+
+# 상 하 좌 우 좌상 좌하 우상 우하
+di = [1, -1, 0, 0, 1, -1, 1, -1]
+dj = [0, 0, -1, 1, -1, -1, 1, 1]
 while True:
     w, h = map(int, input().split())
     ans = 0
     if w == 0 and h == 0:
         break
-    arr = [[0] * h for _ in range(w)]
-    for i in range(w):
-        for j in range(h):
-            pass
+    arr = [list(map(int, input().split())) for _ in range(h)]
+    visited = [[0] * w for _ in range(h)]
+    for i in range(h):
+        for j in range(w):
+            if arr[i][j] == 1 and not visited[i][j]:
+                visited[i][j] = 1
+                dfs(i, j)
+                ans += 1
+    print(ans)
