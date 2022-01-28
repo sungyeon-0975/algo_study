d12ffb5e7dbe717c93c22b7797a350d04e7fc34b
jiyoon <yoonnotyoun@gmail.com>
2022-01-28 22:40:05 +0900

    0128 yoon

--- 220203/18222_투에모스문자열_yoon.py
+++ 220203/18222_투에모스문자열_yoon.py
@@ -1,17 +1,11 @@
-# import sys
-# sys.stdin = open('4963_input.txt')
-# input = sys.stdin.readline
+def sol(n):
+    if n < 2:
+        return n % 2
+    if n % 2:
+        return 1 - sol(n//2)
+    else:
+        return sol(n//2)
 
-# 시간초과
 
 k = int(input())
-a = '0'
-b = ''
-while len(a) < k:
-    for i in range(len(a)):
-        if a[i] == '1':
-            b += '0'
-        else:
-            b += '1'
-    a += b
-print(a[k-1])
+print(sol(k-1))
