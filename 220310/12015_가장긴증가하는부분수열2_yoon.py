1ce25ed0f79c246d27de0aa21394fb8a1c6738b3
jiyoon <yoonnotyoun@gmail.com>
2022-03-09 00:00:16 +0900

    0309 yoon

--- 220310/12015_가장긴증가하는부분수열2_yoon.py
+++ 220310/12015_가장긴증가하는부분수열2_yoon.py
@@ -2,3 +2,24 @@ import sys
 sys.stdin = open('12015_input.txt')
 # input = sys.stdin.readline
 
+# 143064KB / 4888ms
+
+A = int(input())
+numbers = [0] + list(map(int, input().split()))
+ans = [0]
+
+for num in numbers:
+    if ans[-1] < num:
+        ans.append(num)
+    else:
+        left = 0
+        right = len(ans)
+        while left < right:
+            mid = (left + right) // 2
+            if ans[mid] < num:
+                left = mid + 1
+            else:
+                right = mid
+        ans[right] = num
+
+print(len(ans)-1)
