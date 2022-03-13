2a31ff1f3bacf30af64d7dd80004597c726e2de1
jiyoon <yoonnotyoun@gmail.com>
2022-03-13 23:59:48 +0900

    0313 yoon

--- 220317/1051_숫자정사각형_yoon.py
+++ 220317/1051_숫자정사각형_yoon.py
@@ -1,3 +1,27 @@
 import sys
 sys.stdin = open('1051_input.txt')
 # input = sys.stdin.readline
+
+# 30864KB / 68ms
+
+N, M = map(int, input().split())
+arr = [[0] * M for _ in range(N)]
+ans = 1
+for i in range(N):
+    numbers = input()
+    for j in range(M):
+        arr[i][j] = int(numbers[j])
+
+stan = N if N < M else M
+flag = False
+for k in range(stan-1, 0, -1):
+    for i in range(N-k):
+        for j in range(M-k):
+            if arr[i][j] == arr[i+k][j] == arr[i+k][j+k] == arr[i][j+k]:
+                ans = (k+1)**2
+                flag = True
+            if flag: break
+        if flag: break
+    if flag: break
+
+print(ans)
