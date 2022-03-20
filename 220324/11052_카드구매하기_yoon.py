6bf2fe185faa462ef5108c712248d1ffcfed5540
jiyoon <yoonnotyoun@gmail.com>
2022-03-20 18:30:23 +0900

    0320 yoon

--- 220324/11052_카드구매하기_yoon.py
+++ 220324/11052_카드구매하기_yoon.py
@@ -1,3 +1,16 @@
 import sys
 sys.stdin = open('11052_input.txt')
 # input = sys.stdin.readline
+
+#
+
+N = int(input())
+packs = [0] + list(map(int, input().split()))
+dp = [0] * (N+1)
+dp[1] = packs[1]
+for i in range(2, N+1):
+    pos = [packs[i]]
+    for j in range(1, i):
+        pos.append(dp[j] + dp[i-j])
+    dp[i] = max(pos)
+print(dp[N])
