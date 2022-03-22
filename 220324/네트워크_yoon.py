d0d7b70fd0076c1a15bf75083c61264f92d68f30
jiyoon <yoonnotyoun@gmail.com>
2022-03-22 20:19:23 +0900

    0322 yoon

---
+++ 220324/네트워크_yoon.py
@@ -0,0 +1,12 @@
+def solution(n, computers):
+    answer = 0
+    visited = [[0] * n for _ in range(n)]
+    def dfs(k):
+        if k == n:
+
+    for i in range(n):
+        pass
+    return answer
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
