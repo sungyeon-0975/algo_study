3a3ffaa8b6eb445c5c85312d3a50e971518afcc2
jiyoon <yoonnotyoun@gmail.com>
2022-03-24 01:17:15 +0900

    0324 yoon

--- 220324/네트워크_yoon.py
+++ 220324/네트워크_yoon.py
@@ -1,12 +1,17 @@
 def solution(n, computers):
     answer = 0
     visited = [[0] * n for _ in range(n)]
-    def dfs(k):
-        nonlocal answer
-        if k == n:
-            answer += 1
-    for i in range(n):
-        pass
+    def dfs(i):
+        nonlocal answer, visited
+        for j in range(n):
+            if computers[i][j] == 1 and not visited[j]:
+                visited[j] = 1
+                dfs(j)
+                visited[j] = 0
+        answer += 1
+    for k in range(n):
+        if not visited[k]:
+            dfs(k)
     return answer
 
 print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
