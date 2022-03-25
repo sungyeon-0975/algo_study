a042776484f815849ff543964622bd85fef6bb28
jiyoon <yoonnotyoun@gmail.com>
2022-03-26 01:19:26 +0900

    0326 yoon

--- 220331/14425_문자열집합_yoon.py
+++ 220331/14425_문자열집합_yoon.py
@@ -1,3 +1,18 @@
+from collections import defaultdict
 import sys
 sys.stdin = open('14425_input.txt')
 # input = sys.stdin.readline
+
+# 38040KB / 184ms
+
+N, M = map(int, input().split())
+S = defaultdict(int)
+ans = 0
+for _ in range(N):
+    w1 = input().strip()
+    S[w1] += 1
+for _ in range(M):
+    w2 = input().strip()
+    if S[w2] > 0:
+        ans += 1
+print(ans)
